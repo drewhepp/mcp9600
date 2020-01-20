fff58f3a70b5fde9585c63434372340358ac8e06
Andrew Hepp <drewhepp@umich.edu>
2020-01-19 21:06:03 -0500

    attempted to use embedded_hal and shared_bus

--- src/lib.rs
+++ src/lib.rs
@@ -1,98 +1,106 @@
-#[cfg(test)]
-mod tests {
-    use super::*;
+extern crate embedded_hal as hal;
+extern crate shared_bus;
 
-    #[test]
-    fn new_sets_state() {
-        const i2c_addr : u8 = 0x66;
-        const thermo_type : ThermocoupleType = ThermocoupleType::TypeK;
-        const filter_coeffs : FilterCoefficients = 2;
-        
-        let mut i2c_slave_mock = I2cSlave::new(i2c_addr);
-        let mcp9600 = Mcp9600::new(thermo_type, filter_coeffs, i2c_slave_mock);
+pub mod mcp9600 {
 
-        assert!(mcp9600.thermo_type == thermo_type);
-        assert!(mcp9600.filter_coeffs == filter_coeffs);
-    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        use i2cmock::*;
 
-    #[test]
-    fn read_temp_reads_temp() {
-        const i2c_addr : u8 = 0x66;
-        const thermo_type : ThermocoupleType = ThermocoupleType::TypeK;
-        const filter_coeffs : FilterCoefficients = 2;
-        
-        let mut i2c_slave_mock = I2cSlave::new(i2c_addr);
-        let mcp9600 = Mcp9600::new(thermo_type, filter_coeffs, i2c_slave_mock);
+        #[test]
+        fn new_sets_state() {
+            const thermo_type : ThermocoupleType = ThermocoupleType::TypeK;
+            const filter_coeffs : FilterCoefficients = 2;
 
-        let temp_in = 0x12;
-        i2c_slave_mock.write(0x0, temp_in);
+            let i2c = I2cMock::new();
+            let i2c_manager = shared_bus::BusManager::<std::sync::Mutex<_>, _>::new(i2c);
+            
+            let mcp9600 = Mcp9600::new(i2c_manager.acquire(), thermo_type, filter_coeffs);
 
-        let temp_out = mcp9600.read_temp();
-        assert!(temp_out == temp_in);
-    }
-}
+            assert!(mcp9600.thermo_type == thermo_type);
+            assert!(mcp9600.filter_coeffs == filter_coeffs);
+        }
 
-struct I2cSlave {
-    addr : u8,
-    registers : [u32; 255],
-}
-impl I2cSlave {
-    pub fn new(addr : u8) -> I2cSlave {
-        I2cSlave {
-            addr,
-            registers : [0; 255]
+        #[test]
+        fn read_temp_reads_temp() {
+            const thermo_type : ThermocoupleType = ThermocoupleType::TypeK;
+            const filter_coeffs : FilterCoefficients = 2;
+
+            let i2c = I2cMock::new();
+            let i2c_manager = shared_bus::BusManager::<std::sync::Mutex<_>, _>::new(i2c);
+            let mcp9600 = Mcp9600::new(i2c_manager.acquire(), thermo_type, filter_coeffs);
+
+            let i2c_mock_handle = i2c_manager.acquire();
+            let temp_in = 0x12;
+            // TODO write temp_in to reg 0x00
+
+            let temp_out = mcp9600.read_temp();
+            assert!(temp_out == temp_in);
+        }
+
+        pub mod i2cmock {
+            pub enum Error {
+                Generic,
+            }
+            pub struct I2cMock {
+                data : u8,
+            }
+            impl hal::blocking::i2c::Write for I2cMock {
+                type Error = Error;
+                fn write(&mut self, addr: u8, bytes: &[u8]) -> Result<(), Self::Error> {
+                    Err(Error::Generic);
+                }
+            }
+            impl hal::blocking::i2c::Read for I2cMock {
+                type Error = Error;
+                fn read(&mut self, addr: u8, bytes: &mut [u8]) -> Result<(), Self::Error> {
+                    Err(Error::Generic);
+                }
+            }
+            impl I2cMock {
+                pub fn new() -> I2cMock {
+                    return I2cMock{ data : 1}
+                }
+            }
+
         }
     }
-    pub fn read(&self, ptr : usize) -> u32 {
-        return self.registers[ptr];
+
+    pub type Temperature = u32; // 32 bit fixed point
+    pub type FilterCoefficients = isize; // todo
+        
+    #[derive(PartialEq)]
+    pub enum ThermocoupleType {
+        TypeK,
     }
-    pub fn write(&mut self, ptr : usize, data : u32) {
-        self.registers[ptr] = data;
+        
+    struct Status {
+        burst_complete : bool,
+        hot_junction_temperature_updated : bool,
+        input_range : bool,
+        alert_4_status : bool,
+        alert_3_status : bool,
+        alert_2_status : bool,
+        alert_1_status : bool,
     }
-}
 
-type Temperature = u32; // 32 bit fixed point
-type FilterCoefficients = isize; // todo
-    
-#[derive(PartialEq)]
-enum ThermocoupleType {
-    TypeK,
-}
+    pub struct Mcp9600 {
+        thermo_type : ThermocoupleType,
+        filter_coeffs : FilterCoefficients,
 
-#[derive(PartialEq)]
-enum Alert {
-    Alert1,
-    Alert2,
-    Alert3,
-    Alert4,
-}
-    
-struct Status {
-    burst_complete : bool,
-    hot_junction_temperature_updated : bool,
-    input_range : bool,
-    alert_4_status : bool,
-    alert_3_status : bool,
-    alert_2_status : bool,
-    alert_1_status : bool,
-}
+    }
 
-struct Mcp9600 {
-    thermo_type : ThermocoupleType,
-    filter_coeffs : FilterCoefficients,
-    i2c_slave : &I2cSlave,
-}
-
-impl Mcp9600 {
-    pub fn new(thermo_type : ThermocoupleType, filter_coeffs : FilterCoefficients, i2c_slave : &I2cSlave) -> Mcp9600 {
-        Mcp9600 {
-            thermo_type,
-            filter_coeffs,
-            i2c_slave,
+    impl Mcp9600 {
+        pub fn new<T: hal::blocking::i2c::Write + hal::blocking::i2c::Read>
+            (i2c_bus: T, thermo_type: ThermocoupleType, filter_coeffs: FilterCoefficients) -> Mcp9600 {
+            return Mcp9600 {
+                thermo_type,
+                filter_coeffs,
+            }
+        }
+        pub fn read_temp(&self) -> Temperature {
+            return 1;
         }
     }
-    pub fn read_temp(&self) -> Temperature {
-        return 1;
-    }
-    pub fn set_alert(&self, alert : Alert) {}
 }
